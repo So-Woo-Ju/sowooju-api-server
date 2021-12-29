4f38c24b379b1e9ffbc9ebf6eb2d5ed47ebe9c0e
haron47 <pooh0407@ewhain.net>
2021-12-29 14:02:46 +0900

    style : apply google convention

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,25 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    'google',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '**/migrations/*', '**/seed/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'new-cap': 0,
+    'require-jsdoc': 0,
+    'no-unused-vars': 'off',
+    strict: 0,
   },
 };
