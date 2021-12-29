689e8a67784077f134a1f4bb750120f5821412c4
haron47 <pooh0407@ewhain.net>
2021-12-29 19:09:32 +0900

    feat : create error code

--- src/httpException.filter.ts
+++ src/httpException.filter.ts
@@ -1,20 +1,22 @@
-import {ArgumentsHost, Catch, ExceptionFilter, HttpException} from '@nestjs/common';
+import {ExceptionFilter, Catch, ArgumentsHost, HttpException} from '@nestjs/common';
 import {Response} from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost): any {
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const err = exception.getResponse() as
-      | {message: any; statusCode: number}
-      | {error: string; statusCode: 400; message: string[]};
-
-    response.status(status).json({
-      code: status,
-      message: err.message,
-      data: null,
-    });
+      | {message: any; code: number}
+      | {error: string; message: string[]; code: number | null};
+    if (err.code) {
+      return response.status(status).json({
+        success: false,
+        code: err.code,
+        data: err.message,
+      });
+    }
+    response.status(status).json({success: false, code: status, data: err.message});
   }
 }
