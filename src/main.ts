2b6c1d31173841ff3839c1e7894342e8c1ebc78a
haron47 <pooh0407@ewhain.net>
2021-12-29 19:12:29 +0900

    feat : create interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {setupSwagger} from './swagger/index';
 import {HttpExceptionFilter} from './httpException.filter';
+import {TransformInterceptor} from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
