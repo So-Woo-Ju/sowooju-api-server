2e8db6141eaf18fc0a33beb7edaafbbea327534f
haron47 <pooh0407@ewhain.net>
2021-12-30 10:06:42 +0900

    chore : remove spelling error

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {setupSwagger} from './swagger/index';
 import {HttpExceptionFilter} from './httpException.filter';
 import {TransformInterceptor} from './transform.interceptor';
-import {API_PREFIX} from './constants';
+import {API_PREFIX, DOC_PATH} from './constants';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
   app.setGlobalPrefix(API_PREFIX);
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('SoWooJu API docs')
+    .setDescription('The SoWooJu API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(DOC_PATH, app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
