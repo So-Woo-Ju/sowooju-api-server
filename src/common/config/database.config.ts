11780671d87c753d0f7c36fc9595ab24e0c6f570
haron47 <pooh0407@ewhain.net>
2021-12-31 08:28:16 +0900

    feat : add config service

---
+++ src/common/config/database.config.ts
@@ -0,0 +1,8 @@
+import {registerAs} from '@nestjs/config';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+export default registerAs('database', async (): Promise<TypeOrmModuleOptions> => {
+  return {
+    ...require('../../../ormconfig'),
+  };
+});
