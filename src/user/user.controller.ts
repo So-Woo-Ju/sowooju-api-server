6c3bd65f7832a4ddb512b336424299a375088859
Somi Kim <rlathal009@gmail.com>
2022-01-02 01:14:34 -0800

    feat : create UserController code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
 import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 import {UserService} from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+  @Post()
+  async create(@Body() CreateUserDto: CreateUserDto): Promise<User>{
+    return await this.userService.create(CreateUserDto);
+  }
 }
