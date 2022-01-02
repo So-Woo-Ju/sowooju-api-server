8215cc8d00c3e1ac85a68b66f1d7d6e721547a55
Somi Kim <rlathal009@gmail.com>
2022-01-02 01:04:22 -0800

    feat : add createUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { IsDate, IsEmail, IsInt, IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsInt()
+  id: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  googleAccount: string;
+
+  @IsString()
+  kakaoAccount: string;
+
+  @IsString()
+  refreshToken: string;
+
+  @IsDate()
+  createdDate: Date;
+
+  @IsDate()
+  updateDate: Date;
+}
