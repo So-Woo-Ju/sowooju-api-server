58dd64909e12fca2c3b260d2d576ae8ea12d38d1
Somi Kim <rlathal009@gmail.com>
2022-01-04 13:50:03 -0800

    feat : add password hashing code

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {User} from './entities/user.entity';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {CreateUserDto} from './dto/create-user.dto';
+import {Err} from 'src/common/error';
 
 @Injectable()
 export class UserService {
@@ -11,24 +12,20 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  findAll(): Promise<User[]> {
-    console.log('유저 전체 불러오기');
-    return this.userRepository.find();
-  }
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const {email, password} = createUserDto;
 
-  async create(userData: CreateUserDto): Promise<User> {
-    const {id, email, password, googleAccount, kakaoAccount, refreshToken, createdAt, updatedAt} = userData;
-
+    const existingEmail = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (existingEmail) {
+      throw new BadRequestException(Err.USER.EXISTING_USER);
+    }
     const user = new User();
-    user.id = id;
     user.email = email;
     user.password = password;
-    user.googleAccount = googleAccount;
-    user.kakaoAccount = kakaoAccount;
-    user.refreshToken = refreshToken;
-    user.createdAt = createdAt;
-    user.updatedAt = updatedAt;
-
     await this.userRepository.save(user);
     user.password = undefined;
     console.log(user);
