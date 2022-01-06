dd807002c0721548f9d73fe0b9262f480507eb68
haron47 <pooh0407@ewhain.net>
2022-01-06 23:42:50 +0900

    chore: change error messge

--- src/common/error.ts
+++ src/common/error.ts
@@ -6,7 +6,7 @@ export const Err = {
     },
     NOT_FOUND: {
       code: 400,
-      message: '사용자가 존재하지 않습니다.',
+      message: '존재하지 않는 사용자입니다.',
     },
     EXISTING_USER: {
       code: 400,
@@ -56,7 +56,7 @@ export const Err = {
     },
     NOT_SEND_MAIL_ERROR: {
       code: 500,
-      message: '메일 전송 중 에러가 발생하였습니다 ',
+      message: '메일 전송 중 에러가 발생하였습니다.',
     },
   },
 };
