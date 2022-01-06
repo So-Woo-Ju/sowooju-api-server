4dcafe417ba591b3b43cbaae81f164770754d73f
haron47 <pooh0407@ewhain.net>
2022-01-07 00:12:40 +0900

    chore: remove aes key value

--- src/common/config/auth.config.ts
+++ src/common/config/auth.config.ts
@@ -3,7 +3,6 @@ import {registerAs} from '@nestjs/config';
 export default registerAs('auth', () => {
   return {
     secret: process.env.JWT_SECRET_KEY,
-    aesKey: process.env.AES_KEY,
     accessTokenExp: '15m',
     refreshTokenExp: '30d',
   };
