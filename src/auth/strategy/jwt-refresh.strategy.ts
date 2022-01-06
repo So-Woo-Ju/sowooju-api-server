84aaa8f59e936471103078193492bc49c1e8ea3e
haron47 <pooh0407@ewhain.net>
2022-01-06 23:44:01 +0900

    feat: create jwt refresh token strategy / guard

---
+++ src/auth/strategy/jwt-refresh.strategy.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@nestjs/common';
+import {PassportStrategy} from '@nestjs/passport';
+import {ExtractJwt, Strategy} from 'passport-jwt';
+import {ConfigService} from '@nestjs/config';
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('auth').secret,
+    });
+  }
+}
