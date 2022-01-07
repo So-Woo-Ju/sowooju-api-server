0126cd221e5bddf7894a4f50900e74d481ee5710
haron47 <pooh0407@ewhain.net>
2022-01-08 00:50:15 +0900

    feat: change jwt refresh guard logic

--- src/auth/guard/jwt-refresh.guard.ts
+++ src/auth/guard/jwt-refresh.guard.ts
@@ -5,54 +5,37 @@ import {
   ServiceUnavailableException,
 } from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
-import {AuthService} from '../auth.service';
-import {UserService} from './../../user/user.service';
 import {Err} from './../../common/error';
 
 @Injectable()
 export class JwtRefreshGuard extends AuthGuard('jwt-refresh') {
-  constructor(private authService: AuthService, private userService: UserService) {
+  constructor() {
     super();
   }
 
-  async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    const {authorization} = request.headers;
-
-    if (authorization === undefined) {
-      throw new BadRequestException(Err.TOKEN.NOT_SEND_REFRESH_TOKEN);
-    }
-
-    const refreshToken = authorization.replace('Bearer ', '');
-    const refreshTokenValidate = await this.validate(refreshToken);
-    request.user = refreshTokenValidate;
-    return true;
-  }
-
-  async validate(refreshToken: string) {
-    try {
-      const tokenVerify = await this.authService.tokenValidate(refreshToken);
-      const user = await this.userService.findUserById(tokenVerify.sub);
+  handleRequest(err, user, info, context: ExecutionContext) {
+    if (user) {
+      const {authorization} = context.switchToHttp().getRequest().headers;
+      const refreshToken = authorization.replace('Bearer ', '');
       if (user.refreshToken === refreshToken) {
+        delete user.refreshToken;
         return user;
       } else {
         throw new BadRequestException(Err.TOKEN.NO_PERMISSION);
       }
-    } catch (error) {
-      switch (error.message) {
-        // 토큰에 대한 오류를 판단합니다.
-        case 'invalid token':
-          throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
+    }
+    switch (info.toString()) {
+      case 'Error: No auth token':
+        throw new BadRequestException(Err.TOKEN.NOT_SEND_TOKEN);
 
-        case 'no permission':
-          throw new BadRequestException(Err.TOKEN.NO_PERMISSION);
+      case 'JsonWebTokenError: invalid signature':
+        throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
 
-        case 'jwt expired':
-          throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
+      case 'TokenExpiredError: jwt expired':
+        throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
 
-        default:
-          throw new ServiceUnavailableException(Err.SERVER.UNEXPECTED_ERROR);
-      }
+      default:
+        throw new ServiceUnavailableException(Err.SERVER.UNEXPECTED_ERROR);
     }
   }
 }
