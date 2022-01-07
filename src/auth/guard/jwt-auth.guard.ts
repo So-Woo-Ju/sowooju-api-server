2fbcfeed7cfad09b8edd739d656938b4434a9eb5
haron47 <pooh0407@ewhain.net>
2022-01-08 00:49:36 +0900

    feat: add access token error

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,5 +1,31 @@
-import {Injectable} from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  ServiceUnavailableException,
+  ExecutionContext,
+  Head,
+} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
+import {Err} from './../../common/error';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info) {
+    if (user) {
+      return user;
+    }
+    switch (info.toString()) {
+      case 'Error: No auth token':
+        throw new BadRequestException(Err.TOKEN.NOT_SEND_TOKEN);
+
+      case 'JsonWebTokenError: invalid signature':
+        throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
+
+      case 'TokenExpiredError: jwt expired':
+        throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
+
+      default:
+        throw new ServiceUnavailableException(Err.SERVER.UNEXPECTED_ERROR);
+    }
+  }
+}
