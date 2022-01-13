696c34befed2895e26a14159fea8686a10f4ad2f
Sohyun-Dev <jf32430413@gmail.com>
2022-01-13 11:07:09 +0900

    feat : create google auth code

---
+++ src/auth/strategy/google.strategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from './../auth.service';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: 'http://localhost:3000/user/auth/google/callback',
+      scope: ['email'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const googleId = profile.id;
+    const user = await this.authService.validateGoogle(googleId);
+    if (user === null) {
+      return { googleId, type: 'google' };
+    }
+
+    return { user, type: 'login' };
+  }
+}
