1107fa3ba3db8b7f8fafd65f975b268e276c3a8e
hocaron <pooh0407@ewhain.net>
2022-02-21 01:53:16 +0900

    chore: change s3 to  aws s3 constant

--- src/common/config/s3.confilg.ts
+++ src/common/config/s3.confilg.ts
@@ -1,12 +1,13 @@
 import {registerAs} from '@nestjs/config';
-import * as AWS from 'aws-sdk';
+import {config, S3} from 'aws-sdk';
+import {AWS_S3} from './../../constants';
 
-export default registerAs('s3', async (): Promise<AWS.S3> => {
-  AWS.config.update({
+export default registerAs(AWS_S3, async (): Promise<AWS.S3> => {
+  config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     signatureVersion: 'v4',
     region: 'ap-northeast-2',
   });
-  return new AWS.S3({useAccelerateEndpoint: true});
+  return new S3({useAccelerateEndpoint: true});
 });
