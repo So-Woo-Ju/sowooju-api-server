ca75e1aeb6f77de1ab00924d54557cd21ab08582
hocaron <pooh0407@ewhain.net>
2022-02-20 21:58:49 +0900

    chore: add aws s3 config option

--- src/common/config/s3.confilg.ts
+++ src/common/config/s3.confilg.ts
@@ -5,6 +5,8 @@ export default registerAs('s3', async () => {
   AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    signatureVersion: 'v4',
+    region: 'ap-northeast-2',
   });
   return new AWS.S3({useAccelerateEndpoint: true});
 });
--- src/media/dto/get-presigned-url.dto.ts
+++
@@ -1,6 +0,0 @@
-import {ApiProperty} from '@nestjs/swagger';
-
-export class GetPresignedUrlDto {
-  @ApiProperty({example: '123456789'})
-  contentType: string;
-}
