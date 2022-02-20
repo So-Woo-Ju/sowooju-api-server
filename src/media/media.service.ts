95de8e085343bc0c9348fac1a64f65a81a6cdff5
hocaron <pooh0407@ewhain.net>
2022-02-20 11:28:44 +0900

    feat: create get presigned url service

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,11 +1,90 @@
 import {Injectable} from '@nestjs/common';
-import {CreateMediaDto} from './dto/create-media.dto';
-import {UpdateMediaDto} from './dto/update-media.dto';
+import {ConfigService} from '@nestjs/config';
+import {GetPresignedUrlDto} from './dto/get-presigned-url.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Media} from './entities/media.entity';
+import {Repository} from 'typeorm';
 
 @Injectable()
 export class MediaService {
-  create(createMediaDto: CreateMediaDto) {
-    return 'This action adds a new media';
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Media)
+    private readonly mediaRepository: Repository<Media>,
+  ) {}
+
+  async getPresignedUrl(userId: number, contentType: string, bucketName: string) {
+    const s3 = this.configService.get('s3');
+
+    const filetype = contentType.split('/')[1];
+    const fileName = `${userId}.${filetype}`;
+
+    const params = {
+      Bucket: bucketName,
+      Key: fileName,
+      Expires: 3600,
+      ContentType: contentType,
+      ACL: 'public-read',
+    };
+    const s3Url = await s3.getSignedUrlPromise('putObject', params);
+
+    return s3Url;
+  }
+
+  async giveVideoPresignedUrl(userId: number, getPresignedUrlDto: GetPresignedUrlDto) {
+    /*
+    TODO content type이 video type과 맞지 않는 경우 에러 핸들링
+    */
+
+    const videoS3BucketName = this.configService.get('s3-bucket').videoS3BucketName;
+    const videoS3Url = await this.getPresignedUrl(
+      userId,
+      getPresignedUrlDto.contentType,
+      videoS3BucketName,
+    );
+    return videoS3Url;
+  }
+
+  async giveCaptioPresignedUrl(userId: number, getPresignedUrlDto: GetPresignedUrlDto) {
+    /*
+    TODO content type이 caption type과 맞지 않는 경우 에러 핸들링
+    */
+
+    const captionS3BucketName = this.configService.get('s3-bucket').captionS3BucketName;
+    const captionS3Url = await this.getPresignedUrl(
+      userId,
+      getPresignedUrlDto.contentType,
+      captionS3BucketName,
+    );
+    return captionS3Url;
+  }
+
+  async giveTextPresignedUrl(userId: number, getPresignedUrlDto: GetPresignedUrlDto) {
+    /*
+    TODO content type이 text type과 맞지 않는 경우 에러 핸들링
+    */
+
+    const textS3BucketName = this.configService.get('s3-bucket').textS3BucketName;
+    const textS3Url = await this.getPresignedUrl(
+      userId,
+      getPresignedUrlDto.contentType,
+      textS3BucketName,
+    );
+    return textS3Url;
+  }
+
+  async giveThumbnailPresignedUrl(userId: number, getPresignedUrlDto: GetPresignedUrlDto) {
+    /*
+    TODO content type이 thumbnail type과 맞지 않는 경우 에러 핸들링
+    */
+
+    const thumbnailS3BucketName = this.configService.get('s3-bucket').thumbnailS3BucketName;
+    const thumbnailS3Url = await this.getPresignedUrl(
+      userId,
+      getPresignedUrlDto.contentType,
+      thumbnailS3BucketName,
+    );
+    return thumbnailS3Url;
   }
 
   findAll() {
@@ -14,10 +93,6 @@ export class MediaService {
 
   findOne(id: number) {
     return `This action returns a #${id} media`;
-  }
-
-  update(id: number, updateMediaDto: UpdateMediaDto) {
-    return `This action updates a #${id} media`;
   }
 
   remove(id: number) {
