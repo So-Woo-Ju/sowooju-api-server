9962d7ffccd296b9ac2216d5d195c277a3577948
SunwooHo <pooh0407@ewhain.net>
2022-05-09 16:30:26 +0900

    Revert "Feature/pub sub"

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(DOC_PATH, app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
