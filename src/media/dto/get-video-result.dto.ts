221edc9120b064e993d4a583f05a4fa57e9717b3
Somi <rlathal009@gmail.com>
2022-05-25 21:00:11 +0900

    feat : create get-video-result dto

---
+++ src/media/dto/get-video-result.dto.ts
@@ -0,0 +1,36 @@
+import {ApiProperty} from '@nestjs/swagger';
+import {BaseResponseDto} from 'src/common/dto/base-response.dto';
+import {VideoLanguage, VideoType} from '../entities/media.entity';
+
+export class GetVideoResultDto {
+  @ApiProperty({example: 1})
+  userId: number;
+
+  @ApiProperty({example: '비디오'})
+  videoName: string;
+
+  @ApiProperty({example: 'LOCAL'})
+  videoType: VideoType;
+
+  @ApiProperty({example: 'KOR'})
+  videoLanguage: VideoLanguage;
+
+  @ApiProperty({example: 'Saved File Name'})
+  fileName: string;
+}
+
+export class GetVideoResultResponseDto {
+  @ApiProperty({example: 'https://bucket-name.s3-accelerate.amazonaws.com...'})
+  captionUrl: string;
+
+  @ApiProperty({example: 'https://bucket-name.s3-accelerate.amazonaws.com...'})
+  textUrl: string;
+}
+
+export class GetVideoResultResponseBodyDto extends BaseResponseDto {
+  @ApiProperty({example: 200})
+  statusCode: number;
+
+  @ApiProperty()
+  data: GetVideoResultResponseDto;
+}
